b5651f3d2d7c9a35293b565f5cecfffad846f6dd
unknown <ehlek4000@gmail.com>
2021-11-20 15:37:15 +0100

    refactored, save both versions of a song

--- hb_pydub.py
+++ hb_pydub.py
@@ -9,15 +9,7 @@ print("Launching file picker")
 Tk().withdraw()
 filepath = askopenfilename(initialdir = ".",title = "Select file",filetypes = (("supported sound files","*.wav *.mp3 *.ogg *.flv *.wma *.mp4"),("all files","*.*")))
 format = filepath.split(".")[-1]
-if format == "mp3":
-    song = AudioSegment.from_mp3(filepath)
-elif format == "wav":
-    song = AudioSegment.from_wav(filepath)
-elif format == "ogg":
-    song = AudioSegment.from_ogg(filepath)
-elif format == "flv":
-    song = AudioSegment.from_flv(filepath)
-elif format in ["mp4", "wma"]:
+if format in ["mp4", "wma", "mp3", "wav", "ogg", "flv"]:
     song = AudioSegment.from_file(filepath, format)
 else:
     print("not a supported file")
@@ -25,14 +17,19 @@ else:
 sr = 1000
 bpm = int(input("Enter the BPM of the audio file: "))
 usek = (60/bpm)*sr
+print("usek length: " + str(usek))
 pocet_usekov = round(song.duration_seconds*sr/usek)
 print("Processing...")
-newsong = AudioSegment.empty()
+newsong1 = AudioSegment.empty()
+newsong2 = AudioSegment.empty()
 for i in range(pocet_usekov):
     if (i % 2) == 0 and i != 0:
-        newsong = newsong + song[round((i-1)*usek):round(i*usek)]
-output_folder = "./output"
+        newsong1 = newsong1 + song[round((i-1)*usek):round(i*usek)]
+    else:
+        newsong2 = newsong2 + song[round((i-1)*usek):round(i*usek)]
+output_folder = "./half_beat_output"
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
 filename = filepath.split("/")[-1].split(".")[0]
-newsong.export(output_folder + "/hb_" + filename + "." + format, format = format)
+newsong1.export(output_folder + "/hb_" + filename + "a." + format, format = format)
+newsong2.export(output_folder + "/hb_" + filename + "b." + format, format = format)
